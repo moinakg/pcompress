b2cbf0699e05c5b51e6f814aa0d655013fd8e9dd
Moinak Ghosh <moinakg@gmail.com>
2012-11-11 14:58:39 +0530

    Use fixed rolling-hash mask for better block size approximation.

--- rabin/rabin_dedup.h
+++ rabin/rabin_dedup.h
@@ -73,6 +73,7 @@
 #define	RAB_POLYNOMIAL_MIN_WIN_SIZE 8
 #define	RAB_POLYNOMIAL_MAX_WIN_SIZE 64
 #define	RAB_POLYNOMIAL_MAX_BLOCK_SIZE (128 * 1024)
+#define	RAB_BLK_MASK (((1 << RAB_BLK_MIN_BITS) - 1) >> 1)
 
 // Minimum practical chunk size when doing dedup
 #define	RAB_MIN_CHUNK_SIZE (1048576L)
